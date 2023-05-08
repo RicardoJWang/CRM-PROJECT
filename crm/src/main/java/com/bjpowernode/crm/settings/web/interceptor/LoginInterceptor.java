07bb5d365f170e17d2191aabfb3cdea472006fc5
Ricardo.J.Wang <842911438@qq.com>
2023-05-08 09:46:18 +0800

    Create LoginInterceptor.java

---
+++ crm/src/main/java/com/bjpowernode/crm/settings/web/interceptor/LoginInterceptor.java
@@ -0,0 +1,30 @@
+package com.bjpowernode.crm.settings.web.interceptor;
+
+import com.bjpowernode.crm.commons.constants.Constants;
+import com.bjpowernode.crm.settings.domain.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        User user = (User) request.getSession().getAttribute(Constants.SESSION_USER);
+        if (user == null) {
+            response.sendRedirect(request.getContextPath());
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
